1f53b9281bb610cb808e30a1dbf7a3e854e36ea7
Yudo Maryanto <yudo.maryanto@NTB-084.aiia.co.id>
2016-11-16 16:12:29 +0700

    dev-3.0 : print invoice, save dulu

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,6 +18,9 @@ use Libern\QRCodeReader\QRCodeReader;
 use Datatables;
 use App\m_bank;
 use App\t_bank_data;
+
+
+
 class HomeController extends Controller {
 
 	/*
@@ -150,7 +153,7 @@ class HomeController extends Controller {
 		$invoice->due_date 			= $input['due_date'];
 		$invoice->curr 				= $input['curr'];
 		$invoice->amount 			= $input['amount'];
-		$invoice->doc_no_2 			= $input['doc_no_2'];
+		// $invoice->doc_no_2 			= $input['doc_no_2'];
 		$invoice->tgl_input 		= $date;
 		$invoice->status 			= "1";
 		$invoice->no_po 			= $input['no_po'];
@@ -438,34 +441,124 @@ class HomeController extends Controller {
 		return view('invoice.rtp_list_2', compact('user'));
 	}
 
+	function kekata($x) {
+    $x = abs($x);
+    $angka = array("", "satu", "dua", "tiga", "empat", "lima",
+    "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas");
+    $temp = "";
+    if ($x <12) {
+        $temp = " ". $angka[$x];
+    } else if ($x <20) {
+        $temp = $this->kekata($x - 10). " belas";
+    } else if ($x <100) {
+        $temp = $this->kekata($x/10)." puluh". $this->kekata($x % 10);
+    } else if ($x <200) {
+        $temp = " seratus" . $this->kekata($x - 100);
+    } else if ($x <1000) {
+        $temp = kekata($x/100) . " ratus" . $this->kekata($x % 100);
+    } else if ($x <2000) {
+        $temp = " seribu" . $this->kekata($x - 1000);
+    } else if ($x <1000000) {
+        $temp = $this->kekata($x/1000) . " ribu" . $this->kekata($x % 1000);
+    } else if ($x <1000000000) {
+        $temp = $this->kekata($x/1000000) . " juta" . $this->kekata($x % 1000000);
+    } else if ($x <1000000000000) {
+        $temp = $this->kekata($x/1000000000) . " milyar" . $this->kekata(fmod($x,1000000000));
+    } else if ($x <1000000000000000) {
+        $temp = $this->kekata($x/1000000000000) . " trilyun" . $this->kekata(fmod($x,1000000000000));
+    }     
+        return $temp;
+}
+
+	function terbilang($x, $style=4) {
+    if($x<0) {
+        $hasil = "minus ". trim($this->kekata($x));
+    } else {
+        $hasil = trim($this->kekata($x));
+    }     
+    switch ($style) {
+        case 1:
+            $hasil = strtoupper($hasil);
+            break;
+        case 2:
+            $hasil = strtolower($hasil);
+            break;
+        case 3:
+            $hasil = ucwords($hasil);
+            break;
+        default:
+            $hasil = ucfirst($hasil);
+            break;
+    }     
+    return $hasil;
+}
 	public function invoice_print($id)
 	{
 		$invoice = DB::select('select invoice.*, t_bank_datas.*, m_banks.* from invoice inner join t_bank_datas on invoice.code_bank_data = t_bank_datas.id 
 			inner join m_banks on t_bank_datas.code_bank = m_banks.code_bank where invoice.id = "'.$id.'"');
+		 $result = new Collection($invoice);
+		// return $invoice;
+	// $terbilang  = Terbilang::make($amount, ' rupiah');
+
 		
-		\Excel::load('/storage/template/tandaterima.xlsx', function($file) use($invoice){
+		\Excel::load('/storage/template/tandaterima.xlsx', function($file) use($result){
 
-		foreach ($invoice as $invoice) {
+			foreach ($result as $result) {
 			
-			$no_penerimaan = $invoice->no_penerimaan;
-			$vendor_name   = $invoice->vendor_name;
-			$invoice 	   = $invoice->no_po;
-			// $tanggal 	   = $invoice->doc_date;
-			// $keterangan    = $invoice->
-			// $amount        = $invoice->curr;
-			// $ampunt        = $invoice->amount;
-			// $bank_name     = $invoice->bank_name;
-			// $account_no    = $invoice->account_no;
-			// $account_name  = $invoice->account_name;
-			// $jatuh_tempo   = $invoice->due_date;
-			// $tgl           = date("dd-MM-yyyy");
-			
-
-		}
-		$file->setActiveSheetIndex(0)->setCellValue('J7', $no_penerimaan);
-
+				$no_penerimaan = $result->no_penerimaan;
+				$code_vendor   = $result->code_vendor;
+				$vendor_name   = $result->vendor_name;
+				$invoice 	   = $result->doc_no;
+				$tanggal 	   = $result->doc_date;
+				// $keterangan    = $invoice->
+				$curr          = $result->curr;
+				$amount        = $result->amount;
+				$bank_name     = $result->bank_name;
+				$account_no    = $result->account_no;
+				$account_name  = $result->account_name;
+				$jatuh_tempo   = $result->due_date;
+				$tgl           = date("Y-m-d");
+				$tgl2           = date("Y");
+			}
+		$terbilang = $this->terbilang($amount, 1);
+		$terbilang2 = $this->terbilang($amount, 2);
+		//tanda Terima
+		$file->setActiveSheetIndex(0)->setCellValue('K7', $no_penerimaan);
+		$file->setActiveSheetIndex(0)->setCellValue('D9', $no_penerimaan);
+		$file->setActiveSheetIndex(0)->setCellValue('I5', $no_penerimaan);
+		$file->setActiveSheetIndex(0)->setCellValue('D10',$vendor_name);
+		$file->setActiveSheetIndex(0)->setCellValue('B13',$invoice);
+		$file->setActiveSheetIndex(0)->setCellValue('F13',$tanggal);
+		$file->setActiveSheetIndex(0)->setCellValue('L13',$curr." ".number_format($amount, "0", ".", "."));
+		$file->setActiveSheetIndex(0)->setCellValue('L16',$curr." ".number_format($amount, "0", ".", "."));
+		
+		$file->setActiveSheetIndex(0)->setCellValue('D18'," ".$terbilang." RUPIAH");
+		$file->setActiveSheetIndex(0)->setCellValue('K20', $tgl);
+		$file->setActiveSheetIndex(0)->setCellValue('K22', $bank_name);
+		$file->setActiveSheetIndex(0)->setCellValue('K23', $account_no);
+		$file->setActiveSheetIndex(0)->setCellValue('K24', $account_name);
+		$file->setActiveSheetIndex(0)->setCellValue('K25', $jatuh_tempo);
 		// return $no_penerimaan;
-		})->download('pdf');
+
+		//invoice verification voucher
+		$file->setActiveSheetIndex(0)->setCellValue('K33', $tgl);
+		$file->setActiveSheetIndex(0)->setCellValue('E35', $tgl2."/".$no_penerimaan);
+		$file->setActiveSheetIndex(0)->setCellValue('c36', $code_vendor."/".$vendor_name);
+		$file->setActiveSheetIndex(0)->setCellValue('J36', $curr);
+		$file->setActiveSheetIndex(0)->setCellValue('L36', number_format($amount, "0", ".", "."));
+		//keterangan belum
+		$file->setActiveSheetIndex(0)->setCellValue('H39',$invoice);
+		$file->setActiveSheetIndex(0)->setCellValue('L39', number_format($amount, "0", ".", "."));
+		$file->setActiveSheetIndex(0)->setCellValue('C44'," ".$terbilang2." rupiah");
+		$file->setActiveSheetIndex(0)->setCellValue('L44', number_format($amount, "0", ".", "."));
+
+		$file->setActiveSheetIndex(0)->setCellValue('D48', $bank_name);
+		$file->setActiveSheetIndex(0)->setCellValue('D49', $account_no);
+		$file->setActiveSheetIndex(0)->setCellValue('D50', $account_name);
+
+		$file->setActiveSheetIndex(0)->setCellValue('k49', $jatuh_tempo);
+
+		})->download('xlsx');
 	}
 
 	public function invoice_op_user()
